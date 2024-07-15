8e7ee536114414b8c41ed9ab841a33f673ff46f4
Gerard-Yeboua <kragerard2@gmail.com>
2024-07-15 12:48:53 +0000

    Modify

--- index.php
+++ index.php
@@ -21,6 +21,32 @@
 
 <body id="page-top">
 
+    <?php
+    header('Content-Type: application/json');
+
+    // Connexion à la base de données
+    include "db_connexion.php";    
+
+    // Requête SQL pour récupérer les données de transactions
+    $sql = "SELECT month, transactions, earnings, errors FROM transactions";
+    $result = $conn->query($sql);
+
+    $data = [];
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+
+    echo json_encode($data);
+    ?>
+
+
     <!-- Page Wrapper -->
     <div id="wrapper">
 
